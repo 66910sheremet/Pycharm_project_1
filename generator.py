8df364b23624998f1722c0f8bf5d54d429626d1c
Pycharm_project_1 <669101mac@gmail.com>
2021-08-04 08:48:57 +0300

    c Decimal

--- generator.py
+++ generator.py
@@ -1,11 +1,19 @@
 import dictionary
+from decimal import Decimal
 
 # формулы
 # низшая теплота сгорания газа принятого состава Qн:
 
-qn = (0.01 * (int(dictionary.v1) * int(dictionary.c6CH4) + int(dictionary.v2) * int(
-    dictionary.c6CO2))) / 1000000  # Мдж/м3
+qn = Decimal((Decimal('0.01') * (Decimal(dictionary.v1) * Decimal(dictionary.c6CH4) + Decimal(dictionary.v2) * Decimal(
+    dictionary.c6CO2))) / Decimal('1000000'))  # Мдж/м3
 
-# тут нужно дальше расписывать
-
+qn = round(qn, 2)
 print(qn)
+
+# Теоретически необхожимое для горения количество воздуха, м3/м3:
+
+V0 = Decimal((Decimal('0.01') * (Decimal(dictionary.v1) * Decimal(dictionary.c7CH4) + Decimal(dictionary.v2) * Decimal(
+    dictionary.c7CO2))))  # м3/м3:
+
+V0 = round(V0, 2)
+print(V0)
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ import generator
 
 def getResult():
     return print(generator.qn)
+    return print(generator.V0)
